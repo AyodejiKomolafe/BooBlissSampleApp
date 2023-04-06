87cbae6777f00a426cef7b92a73c0b66e984bee2
Ayodeji Komolafe <kvngeko@icloud.com>
2023-04-06 08:55:55 -0400

    Initial Commit

---
+++ BooBlissSample/BooBlissSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooBlissSampleApp.swift
+//  BooBlissSample
+//
+//  Created by Kvng Eko on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct BooBlissSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
