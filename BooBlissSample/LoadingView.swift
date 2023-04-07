7efffe250671530766d51645f78813b6cf77fdac
Ayodeji Komolafe <kvngeko@icloud.com>
2023-04-07 07:30:29 -0400

    loading view completed

--- BooBlissSample/LoadingView.swift
+++ BooBlissSample/LoadingView.swift
@@ -18,3 +18,13 @@ struct ActivityIndicator: UIViewRepresentable {
     func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {
     }
 }
+
+struct LoadingView: View {
+    var body: some View {
+        ZStack {
+            Color(.systemBackground)
+                .edgesIgnoringSafeArea(.all)
+            ActivityIndicator()
+        }
+    } 
+}
