85214653aa560327e55f1aecda099c227e633954
Ayodeji Komolafe <kvngeko@icloud.com>
2023-04-07 06:19:21 -0400

    connecting network manager with list view

--- BooBlissSample/BooBlissSampleListView.swift
+++ BooBlissSample/BooBlissSampleListView.swift
@@ -8,12 +8,30 @@
 import SwiftUI
 
 struct BooBlissSampleListView: View {
+    @State private var menus: [Menu] = []
+    
     var body: some View {
         NavigationView {
             List(MockData.menus) { menu in
                 MenuListCell(menu: menu)
             }
             .navigationTitle("🍟 Menu")
+        }
+        .onAppear{
+            getMenus()
+        }
+    }
+    
+    func getMenus() {
+        NetworkManager.shared.getMenus { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let menus):
+                    self.menus = menus
+                case .failure(let error ):
+                    print(error.localizedDescription)
+                }
+            }
         }
     }
 }
