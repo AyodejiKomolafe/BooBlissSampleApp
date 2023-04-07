9d6b8f6d7ed3a08ef515848bd1f8c0a913918deb
Ayodeji Komolafe <kvngeko@icloud.com>
2023-04-07 07:46:52 -0400

    function to download image completed

--- BooBlissSample/NetworkManager.swift
+++ BooBlissSample/NetworkManager.swift
@@ -5,10 +5,11 @@
 //  Created by Kvng Eko on 4/6/23.
 //
 
-import Foundation
+import UIKit
 
 final class NetworkManager {
     static let shared = NetworkManager()
+    private let cache = NSCache<NSString, UIImage>()
     
     static let baseURL = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals/"
     private let menuURL = baseURL + "appetizers"
@@ -43,6 +44,31 @@ final class NetworkManager {
             } catch {
                 completed(.failure(.invalidData))
             }
+        }
+        task.resume()
+    }
+    
+    func downloadImage(fromURLString urlString: String, completed: @escaping(UIImage?) -> Void ) {
+        let cacheKey = NSString(string: urlString)
+        
+        if let image = cache.object(forKey: cacheKey) {
+            completed(image)
+            return
+        }
+        
+        guard let url = URL(string: urlString) else {
+            completed(nil)
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
+            guard let data = data, let image = UIImage(data: data) else {
+                completed(nil)
+                return
+            }
+            
+            self.cache.setObject(image, forKey: cacheKey)
+            completed(image)
         }
         task.resume()
     }
