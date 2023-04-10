5f6e35331458370802cce7c7374c4eb54f100dc3
Ayodeji Komolafe <kvngeko@icloud.com>
2023-04-10 10:18:53 -0400

    account view structured

--- BooBlissSample/Screens/AccountView/AccountView.swift
+++ BooBlissSample/Screens/AccountView/AccountView.swift
@@ -8,10 +8,40 @@
 import SwiftUI
 
 struct AccountView: View {
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthdate = Date()
+    @State private var extraNapkins = false
+    @State private var frequentRefills = false
+    
     var body: some View {
         NavigationView {
-            Text("Account View")
-                .navigationTitle("💳 Account")
+            Form {
+                Section(header: Text("Personal Info")) {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .textInputAutocapitalization(.none)
+                        .autocorrectionDisabled(true)
+                    DatePicker("Birthday", selection: $birthdate, displayedComponents: .date)
+                    Button {
+                        print("Save")
+                    } label: {
+                        Text("Save Changes")
+                    }
+                }
+                .padding(.leading, -10)
+
+                Section(header: Text("Requests")) {
+                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                    Toggle("Frequent Refills", isOn: $frequentRefills)
+                }
+                .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
+                .padding(.leading, -10)
+            }
+            .navigationTitle("💳 Account")
         }
     }
 }
