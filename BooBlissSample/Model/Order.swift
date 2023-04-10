dc7984a0f00103cc77df97f4b95787a0ba68d5af
Ayodeji Komolafe <kvngeko@icloud.com>
2023-04-10 12:33:28 -0400

    empty state completed

---
+++ BooBlissSample/Model/Order.swift
@@ -0,0 +1,9 @@
+//
+//  Order.swift
+//  BooBlissSample
+//
+//  Created by Kvng Eko on 4/10/23.
+//
+
+import Foundation
+
--- BooBlissSample/Screens/OrderView/OrderView.swift
+++ BooBlissSample/Screens/OrderView/OrderView.swift
@@ -11,26 +11,33 @@ struct OrderView: View {
     @State private var orderItems = MockData.orderItems
     var body: some View {
         NavigationView {
-            VStack {
-                List {
-                    ForEach(orderItems) { menu in
-                        MenuListCell(menu: menu)
+            ZStack {
+                VStack {
+                    List {
+                        ForEach(orderItems) { menu in
+                            MenuListCell(menu: menu)
+                        }
+                        .onDelete { indexSet in
+                            orderItems.remove(atOffsets: indexSet)
+                        }
                     }
-                    .onDelete { indexSet in
-                        orderItems.remove(atOffsets: indexSet)
+                    .listStyle(PlainListStyle())
+                    Button {
+                        print("order placed")
+                    } label: {
+                        MenuButton(title: "$99.99 - Place Order")
                     }
+                    .padding(.bottom, 25)
                 }
-                .listStyle(PlainListStyle())
-                Button {
-                    print("order placed")
-                } label: {
-                    MenuButton(title: "$99.99 - Place Order")
+                if orderItems.isEmpty {
+                    EmptyState(imageName: "empty-order", message: "You have no item in your order. \nPlease add an item from the menu!")
                 }
-                .padding(.bottom, 25)
-
             }
             .navigationTitle("🧾 Orders ")
         }
+    }
+    func deleteItems(at offsets: IndexSet) {
+        orderItems.remove(atOffsets: offsets)
     }
 }
 
