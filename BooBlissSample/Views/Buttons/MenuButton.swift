9d306cafd513e0ee755fe9ffd3e69089ef7d9f7c
Ayodeji Komolafe <kvngeko@icloud.com>
2023-04-10 09:25:15 -0400

    refactoring buttons

---
+++ BooBlissSample/Views/Buttons/MenuButton.swift
@@ -0,0 +1,27 @@
+//
+//  MenuButton.swift
+//  BooBlissSample
+//
+//  Created by Kvng Eko on 4/10/23.
+//
+
+import SwiftUI
+
+struct MenuButton: View {
+    let title: String
+    var body: some View {
+        Text(title)
+            .font(.title2)
+            .fontWeight(.semibold)
+            .frame(width: 260, height: 50)
+            .foregroundColor(.white)
+            .background(Color.brandPrimary)
+            .cornerRadius(15)
+    }
+}
+
+struct MenuButton_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuButton(title: "Test title")
+    }
+}
