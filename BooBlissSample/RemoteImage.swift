0d26faff932f6ad7fdb62ba135385f3cf08b5de1
Ayodeji Komolafe <kvngeko@icloud.com>
2023-04-10 08:19:29 -0400

    remote image structured

--- BooBlissSample/RemoteImage.swift
+++ BooBlissSample/RemoteImage.swift
@@ -7,4 +7,22 @@
 
 import SwiftUI
 
- 
+final class ImageLoader: ObservableObject {
+    @Published var image: Image? = nil
+    
+    func load(fromURLString urlstring: String) {
+        NetworkManager.shared.downloadImage(fromURLString: urlstring) { uiImage in
+            guard let uiImage = uiImage else { return }
+            DispatchQueue.main.async {
+                self.image = Image(uiImage: uiImage)
+            }
+        }
+    }
+}
+
+struct RemoteImage: View {
+    var image: Image?
+    var body: some View {
+        image?.resizable()
+    }
+}
